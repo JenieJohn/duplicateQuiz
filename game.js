0f3efea0292b926403dce1886ae49ed067f880af
JenieJohn <52359716+JenieJohn@users.noreply.github.com>
2022-08-06 18:23:25 +0530

    Update game.js

--- game.js
+++ game.js
@@ -3,8 +3,6 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-const loader = document.getElementById('loader');
-const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -13,33 +11,12 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch(
-    'question.json'
-)
+fetch('question.json')
     .then((res) => {
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
-
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-
-            return formattedQuestion;
-        });
-
+        questions = loadedQuestions;
         startGame();
     })
     .catch((err) => {
@@ -47,7 +24,7 @@ fetch(
     });
 
 //CONSTANTS
-const CORRECT_BONUS = 10;
+const CORRECT_BONUS = 5;
 const MAX_QUESTIONS = 7;
 
 startGame = () => {
@@ -55,8 +32,6 @@ startGame = () => {
     score = 0;
     availableQuesions = [...questions];
     getNewQuestion();
-    game.classList.remove('hidden');
-    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
@@ -66,17 +41,17 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = Question ${questionCounter}/${MAX_QUESTIONS};
     //Update the progress bar
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    progressBarFull.style.width = ${(questionCounter / MAX_QUESTIONS) * 100}%;
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
-    question.innerHTML = currentQuestion.question;
+    question.innerText = currentQuestion.question;
 
     choices.forEach((choice) => {
         const number = choice.dataset['number'];
-        choice.innerHTML = currentQuestion['choice' + number];
+        choice.innerText = currentQuestion['choice' + number];
     });
 
     availableQuesions.splice(questionIndex, 1);
